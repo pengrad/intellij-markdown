ddd86a5f637fc3eecb6bd9836075f04d48420d8f
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2018-03-13 20:43:21 +0300

    switch to kotlin-common model with js and jvm specializations
    * mostly tests are left in common code
    * some specialization code is required
    * gradle script reworked to support deployment of different artifacts

---
+++ src/org/intellij/markdown/html/CommonDefs.kt
@@ -0,0 +1,11 @@
+package org.intellij.markdown.html
+
+expect class URI(str: String) {
+    fun resolve(str: String): URI
+}
+
+expect fun isWhitespace(char: Char): Boolean
+
+expect fun isPunctuation(char: Char): Boolean
+
+expect fun urlEncode(str: String): String
